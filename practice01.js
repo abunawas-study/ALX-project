f53b7d2597a15bc4223ab2875d30e32bc47e8460
Abusadiq Abunawas Nbanba <abunawasabusadiq@gmail.com>
2025-10-09 08:34:51 +0000

    Exercise 1: working with variables and data types

---
+++ practice01.js
@@ -0,0 +1,15 @@
+let name = "Abunawas";
+let age = 20;
+let isStudent = true;
+let address = ("Tamale", "Ghana");
+let hobbies = ["Reading", "Anime", "Coding"];
+let undefinedVariable;
+let nullVariable = null;
+
+console.log(typeof("Name: ", name));
+console.log(typeof("Age: ", age));
+console.log(typeof("Is Student", isStudent));
+console.log(typeof("Address: ", address));
+console.log(typeof("Hobbies: ", hobbies));
+console.log(typeof("Undefined Variable: ", undefinedVariable));
+console.log(typeof("Null Variable: ", nullVariable));
