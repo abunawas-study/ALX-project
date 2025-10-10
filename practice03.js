4b1588faac1bc40065e23651efe89004ad5e149a
Abusadiq Abunawas Nbanba <abunawasabusadiq@gmail.com>
2025-10-10 20:19:31 +0000

    done with 3rd exercise ( basic conditional logic)

---
+++ practice03.js
@@ -0,0 +1,12 @@
+// Practice Problem 3: Conditional logic to check if number is positive, negative, or zero
+
+let number =  -0;
+if (number > 0){
+    console.log( number + "  is a positive number");
+}
+else if (number < 0){
+    console.log(number + " is a negative number");
+}
+else{
+    console.log(number + " is zero");
+}
